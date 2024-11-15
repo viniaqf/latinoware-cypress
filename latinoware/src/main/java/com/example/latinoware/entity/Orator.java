45cdbe2cee2cfde45671b6f08049cc66d3293c81
Vinicius Ferreira <comercial.viniciusgabriel@gmail.com>
2024-11-15 17:18:25 -0300

    feat: Repository created

--- latinoware/src/main/java/com/example/latinoware/entity/Orator.java
+++ latinoware/src/main/java/com/example/latinoware/entity/Orator.java
@@ -1,14 +1,10 @@
 package com.example.latinoware.entity;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.NamedEntityGraph;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.*;
-
-import javax.naming.Name;
 
 @Entity
 @Table(name = "tb_orator")
---
+++ latinoware/src/main/java/com/example/latinoware/repository/OratorRepository.java
@@ -0,0 +1,7 @@
+package com.example.latinoware.repository;
+
+import com.example.latinoware.entity.Orator;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OratorRepository extends JpaRepository<Orator, Long> {
+}
