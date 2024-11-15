b9e503f1fd26ad84b838ae2b125c73d61a1aaf0b
Vinicius Ferreira <comercial.viniciusgabriel@gmail.com>
2024-11-15 16:41:24 -0300

    feat: Event annotations created

--- latinoware/src/main/java/com/example/latinoware/entity/Event.java
+++ latinoware/src/main/java/com/example/latinoware/entity/Event.java
@@ -1,4 +1,31 @@
 package com.example.latinoware.entity;
 
-public class Event {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "clientes", schema = "public")
+@AllArgsConstructor @NoArgsConstructor
+public class Event extends AbstractEntity {
+
+    @Getter @Setter
+    @Column(name = "eventName", nullable = false, unique = true)
+    private String name;
+
+    @Getter @Setter
+    @Column(name = "eventDate", nullable = false)
+    private LocalDateTime date;
+
+    @Getter @Setter
+    @Column(name = "eventLocation", nullable = false)
+    private EventLocation location;
+
+    @Getter @Setter
+    @Column(name = "eventOrator", nullable = false)
+    private Orator orator;
 }
