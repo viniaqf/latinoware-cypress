a94d310c42b1bc0926328fd5903a6b3e30bd33c8
Vinicius Ferreira <comercial.viniciusgabriel@gmail.com>
2024-11-15 15:09:09 -0300

    feat: initializing with spring.io

---
+++ latinoware/src/main/java/com/example/latinoware/LatinowareApplication.java
@@ -0,0 +1,13 @@
+package com.example.latinoware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LatinowareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LatinowareApplication.class, args);
+	}
+
+}
