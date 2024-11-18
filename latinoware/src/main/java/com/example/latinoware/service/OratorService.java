356733575cc48056212cbc08feb67e9957ad4d5f
Vinicius Ferreira <comercial.viniciusgabriel@gmail.com>
2024-11-18 00:17:27 -0300

    feat: OratorService done.

--- latinoware/src/main/java/com/example/latinoware/service/OratorService.java
+++ latinoware/src/main/java/com/example/latinoware/service/OratorService.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 
+import java.util.List;
+
 @Service
 public class OratorService {
     @Autowired
@@ -21,13 +23,40 @@ public class OratorService {
         return modelMapper.map(oratorEnt, OratorDTO.class);
     }
     public OratorDTO post(OratorDTO orator){
-        Assert.hasText(orator.getAge(), "Por favor, coloque uma idade válida!");
         Assert.notNull(orator.getCompanyName(), "Por favor, insira a empresa do Orador.");
         Assert.notNull(orator.getJobTitle(), "Por favor, insira a titulação do Orador");
         Assert.hasText(orator.getJobTitle(), "Por favor, insira uma titulação válida!.");
         return toOratorDTO(repository.save(toOratorEnt(orator)));
     }
+    public OratorDTO put(OratorDTO oratorDTO, Long id){
+        Assert.notNull(id, "Por favor, insira um ID.");
+        Assert.notNull(oratorDTO.getCompanyName(), "Por favor, insira a empresa do Orador.");
+        Assert.notNull(oratorDTO.getJobTitle(), "Por favor, insira a titulação do Orador");
+        Assert.hasText(oratorDTO.getJobTitle(), "Por favor, insira uma titulação válida!.");
+        return toOratorDTO(repository.save(toOratorEnt(oratorDTO)));
+    }
 
+    public OratorDTO findById(Long id) {
+        Orator orator = repository.findById(id).orElse(null);
+        return toOratorDTO(orator);
+    }
+
+    public List<OratorDTO> getAll(){
+        return repository.findAll().stream().map(this::toOratorDTO).toList();
+    }
+
+    public OratorDTO disable (Long id ){
+        OratorDTO oratorDTO = findById(id);
+        oratorDTO.disable();
+        return toOratorDTO(repository.save(toOratorEnt(oratorDTO)));
+    }
+
+    public OratorDTO enable (Long id){
+        OratorDTO oratorDTO = findById(id);
+        oratorDTO.enable();
+        return toOratorDTO(repository.save(toOratorEnt(oratorDTO)));
+    }
+   
 
 
 }
