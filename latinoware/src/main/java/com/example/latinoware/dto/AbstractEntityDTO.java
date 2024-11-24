4298a94b639d44ce267cd3853a8070d184479abd
Vinicius Ferreira <comercial.viniciusgabriel@gmail.com>
2024-11-23 23:14:43 -0300

    fix: updated method created to set and update time.

--- latinoware/src/main/java/com/example/latinoware/dto/AbstractEntityDTO.java
+++ latinoware/src/main/java/com/example/latinoware/dto/AbstractEntityDTO.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.cglib.core.Local;
 
 import java.time.LocalDateTime;
 
@@ -22,7 +23,11 @@ public class AbstractEntityDTO {
     }
     public void enable() {
         this.active = true;
+        this.update = LocalDateTime.now();
     }
 
+    public void update(){
+        this.update = LocalDateTime.now();
+    }
 
 }
